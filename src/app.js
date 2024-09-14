8a3a99503d3c97b72f29713955b1ab403d773954
ademontel <53986476+ademontel@users.noreply.github.com>
2024-09-14 15:01:01 +0000

    primero y último commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,49 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Acá va todo!!!
+  let who = [
+    "El papa Francisco",
+    "Samuel",
+    "El Diegote",
+    "Un vagabundo",
+    "Bob Esponja",
+    "Un Otaku con exceso de cafeína en sangre"
+  ];
+  let action = [
+    "me comió",
+    "orinó sobre",
+    "lloró sobre",
+    "me rompió",
+    "vomitó sobre",
+    "hizo caca sobre"
+  ];
+  let what = [
+    "mis medias",
+    "mi dignidad",
+    "mi teléfono",
+    "mis ideas",
+    "mi castidad",
+    "un tarrito"
+  ];
+  let when = [
+    "en la madrugada",
+    "cuando dormía",
+    "en mi luna de miel",
+    "mientras esperaba el bus",
+    "mientras estaba en el water",
+    "durante un examen"
+  ];
+
+  function getRandom(array) {
+    let randomNumber = Math.floor(Math.random() * array.length);
+    return array[randomNumber];
+  }
+
+  let excusa = `
+  ${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}
+  `;
+  console.log(excusa);
+  document.getElementById("excuse").innerHTML = excusa;
 };
